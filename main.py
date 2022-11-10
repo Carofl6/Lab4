cba7727dc7faf05df8afe2c9b29b0d513123a9ec
Carofl6 <luelmocaro@gmail.com>
2022-11-10 14:23:27 -0600

    c

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 # -- --------------------------------------------------------------------------------------------------- -- #
 # -- project: A SHORT DESCRIPTION OF THE PROJECT                                                         -- #
 # -- script: main.py : python script with the main functionality                                         -- #
-# -- author: YOUR GITHUB USER NAME                                                                       -- #
+# -- author: Carofl6                                                                     -- #
 # -- license: GPL-3.0 License                                                                            -- #
 # -- repository: YOUR REPOSITORY URL                                                                     -- #
 # -- --------------------------------------------------------------------------------------------------- -- #
--- visualizations.py
+++ visualizations.py
@@ -3,7 +3,7 @@
 # -- --------------------------------------------------------------------------------------------------- -- #
 # -- project: A SHORT DESCRIPTION OF THE PROJECT                                                         -- #
 # -- script: visualizations.py : python script with data visualization functions                         -- #
-# -- author: YOUR GITHUB USER NAME                                                                       -- #
+# -- author: Carofl6                                                                       -- #
 # -- license: GPL-3.0 License                                                                            -- #
 # -- repository: YOUR REPOSITORY URL                                                                     -- #
 # -- --------------------------------------------------------------------------------------------------- -- #
